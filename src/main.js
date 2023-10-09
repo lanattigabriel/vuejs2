21e1bfbdfffb0511da11e79e8c2c4f4f97087522
GabiLanatti <83250648+lanattigabriel@users.noreply.github.com>
2023-10-09 07:57:42 -0300

    building resolve

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import '@/assets/bootstrap.min.css'
 import '@/assets/bootstrap.bundle.min.js'
 import '@/assets/custom_styles.css'
